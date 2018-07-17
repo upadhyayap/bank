671f7450c8f3db0802144ddaae136f62046f26ab
Anand <upadhyayap.smit@gmail.com>
2018-07-17 19:23:35 +0530

    added some more api docs for account controller

--- src/main/java/com/banking/bank/controller/AccountController.java
+++ src/main/java/com/banking/bank/controller/AccountController.java
@@ -7,6 +7,8 @@ import com.banking.bank.domain.Transaction;
 import com.banking.bank.services.AccountService;
 import com.banking.bank.services.TransactionService;
 import com.banking.bank.util.status.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -22,6 +24,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/account")
+@Api(value = "Acount Services", description = "This api is responsible for all account related queries.")
 public class AccountController {
 
     public AccountService accountService;
@@ -31,6 +34,7 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @ApiOperation(value = "Open a bank account", code = 200, consumes = "application/json")
     @RequestMapping(value = "/open", method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Resource> create(@RequestBody Account account) {
@@ -40,6 +44,7 @@ public class AccountController {
         return new ResponseEntity<>(resource, status);
     }
 
+    @ApiOperation(value = "Fetch available balance for an account")
     @RequestMapping(value = "/{accountNo}/balance", method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, Double>> getBalance(@PathVariable("accountNo") long accountNo) {
@@ -53,10 +58,12 @@ public class AccountController {
         return new ResponseEntity(resStatus, status);
     }
 
+    @ApiOperation(value = "Add payee in a bank account")
     @RequestMapping(value = "/{accountNo}/addpayee",
             method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResStatus> addPayee(@PathVariable("accountNo") Long accountNo, @RequestBody Map<String, Object> payeeInfo) {
+    public ResponseEntity<ResStatus> addPayee(@PathVariable("accountNo") Long accountNo,
+                                              @RequestBody Map<String, Object> payeeInfo) {
         ResStatus resStatus = new ResStatus();
         boolean status = accountService.addPayee(payeeInfo);
         if (status)
@@ -67,10 +74,12 @@ public class AccountController {
         return new ResponseEntity(resStatus, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Delete Payee in a bank account")
     @RequestMapping(value = "/{accountNo}/droppayee",
             method = RequestMethod.DELETE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity deletePayee(@PathVariable("accountNo") Long accountNo, @RequestBody Map<String, Object> payeeInfo) {
+    public ResponseEntity deletePayee(@PathVariable("accountNo") Long accountNo,
+                                      @RequestBody Map<String, Object> payeeInfo) {
         ResStatus resStatus = new ResStatus();
         boolean status = accountService.deletePayee(payeeInfo);
         if (status)
@@ -81,6 +90,7 @@ public class AccountController {
         return new ResponseEntity(resStatus, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Fetches a transaction history for a bank account")
     @RequestMapping(value = "/{accountNo}/history",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
@@ -90,10 +100,12 @@ public class AccountController {
         return new ResponseEntity<List<Transaction>>(history, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Transfer Fund instantly from one account to another")
     @RequestMapping(value = "/{accountNo}/transferInstant",
             method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Transaction> transferInstant(@PathVariable("accountNo") Long accountNo, @RequestBody Map<String, Object> transferInfo) {
+    public ResponseEntity<Transaction> transferInstant(@PathVariable("accountNo") Long accountNo,
+                                                       @RequestBody Map<String, Object> transferInfo) {
         // catch number format exception and throw error
         Long payeeAccNo = Long.parseLong(transferInfo.get("payerAccNo").toString());
         Double amount = Double.parseDouble(transferInfo.get("amount").toString());
@@ -102,10 +114,12 @@ public class AccountController {
         return new ResponseEntity<Transaction>(transaction, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Schedule fund transfer for a future date")
     @RequestMapping(value = "/{accountNo}/scheduletransfer",
             method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResStatus> scheduleTransact(@PathVariable("accountNo") Long accountNo, @RequestBody Map<String, Object> transferInfo) {
+    public ResponseEntity<ResStatus> scheduleTransact(@PathVariable("accountNo") Long accountNo,
+                                                      @RequestBody Map<String, Object> transferInfo) {
         ResStatus resStatus = new ResStatus();
         boolean transactStatus = accountService.scheduleTransfer(accountNo, transferInfo);
 
@@ -117,8 +131,11 @@ public class AccountController {
         return new ResponseEntity<ResStatus>(resStatus, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/{accountNo}/balance/future", method = RequestMethod.GET)
-    public ResponseEntity<Map<String, Double>> getBalanceForFutureDate(@PathVariable("accountNo") Long accountNo, @RequestParam("date")
+    @ApiOperation(value = "Fetch balance info for a future date")
+    @RequestMapping(value = "/{accountNo}/balance/future", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Double>> getBalanceForFutureDate(@PathVariable("accountNo") Long accountNo,
+                                                                       @RequestParam("date")
     @DateTimeFormat(pattern = "dd-mm-yyyy") Date toDate) {
         Double balance = accountService.getFutureBalance(accountNo, toDate);
         HashMap resStatus = new HashMap();
